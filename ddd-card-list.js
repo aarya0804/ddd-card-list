244b74380659208b8014690df5fdfa385020079b
aarya0804 <ava6560@psu.edu>
2025-03-28 12:13:46 -0400

    removed haxProperties

--- ddd-card-list.js
+++ ddd-card-list.js
@@ -72,13 +72,7 @@ export class DddCardList extends DDDSuper(I18NMixin(LitElement)) {
 </div>`;
   }
 
-  /**
-   * haxProperties integration via file reference
-   */
-  static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
-  }
+  
 }
 
 globalThis.customElements.define(DddCardList.tag, DddCardList);
